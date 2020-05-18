b170ab9297bf989ae8803baf959359e4f64ba07b
Vitor Henrique <vhsouzaeng@gmail.com>
2020-05-18 04:26:55 -0300

    Updates

---
+++ precache-manifest.d6ba980a65b5ae69bd70dca38d141d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b754286ce86cdca23f9063bf60e0b68",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "498eb60c22b390d45743",
+    "url": "/portfolio/static/css/main.0b076e80.chunk.css"
+  },
+  {
+    "revision": "f1ab0f7c19893a5cedb4",
+    "url": "/portfolio/static/js/2.bcc0bd5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.bcc0bd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498eb60c22b390d45743",
+    "url": "/portfolio/static/js/main.993b86e0.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa95eff073a6e2ee6856072c2769ae4d.js"
+  "/portfolio/precache-manifest.d6ba980a65b5ae69bd70dca38d141d3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
