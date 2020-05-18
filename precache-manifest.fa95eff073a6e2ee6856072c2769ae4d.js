a5b3cacf684720e65b75a1011952a2e43510fcfd
Vitor Henrique <vhsouzaeng@gmail.com>
2020-05-18 04:20:21 -0300

    Updates

---
+++ precache-manifest.fa95eff073a6e2ee6856072c2769ae4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f491074ed08a729bd1d9bb52559216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "498eb60c22b390d45743",
+    "url": "/static/css/main.0b076e80.chunk.css"
+  },
+  {
+    "revision": "f1ab0f7c19893a5cedb4",
+    "url": "/static/js/2.bcc0bd5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bcc0bd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498eb60c22b390d45743",
+    "url": "/static/js/main.993b86e0.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
